b18bbc8d6750831c239c8de6919b7252ec979eca
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-04-19 00:05:06 +0900

    black_jack

--- quests/quests_week1-2/quest_blackjack/black_jack/app.mjs
+++ quests/quests_week1-2/quest_blackjack/black_jack/app.mjs
@@ -2,28 +2,77 @@ import { HelperFunctions } from "./components/HelperFunctions.mjs";
 import { Card } from "./components/card.mjs";
 import { Dealer } from "./components/dealer.mjs";
 
-// 52枚のトランプ, 13のランク
-// 1テーブル「ハウス」, 5-7人のプレイヤ
-// 各プレーヤーは「ハウス」と1対1の勝負
-
-// 各プレーヤーの目標: 21を超えないように21に近づけ、その点数がハウスのハンドを上回ること
-// 2-10 -> その手札の点数,  A, J ,Q ,K -> 10 (Aは1か11の都合のいい方でカウント)
-
-// 1, ベット -> 賭け金
-// 2, カードの入手 -> ハウスは各プレーヤーに2枚ずつ配る
-// 3, アクション -> ラウンドロビン形式で維持 or 調整を決める
-//   最初のラウンドでプレーヤーができるアクション -> サレンダー(負けを認める)、スタンド(現在のハンドで勝負)、ヒット(手札に更に1枚追加)、ダブル(ベットを2倍にしてもう一度だけカードを引く)
-//   手札の数が21を超えること -> バスト , プレーヤーがバストしなかったら -> ヒット(もう一度ヒットできる)、スタンド(現在のハンドで勝負できる)
-//   各プレイヤーの全ての行動が終了したタイミング（バスト、スタンド、サレンダー、ダブル）で、
-//   ハウスは裏向きのカードを公開し、それが 17 以上であればスタンド、そうでなければ、17 以上になるまでカードを引き続けなければなりません。
-// 4, 評価 -> プレーヤー勝ち = ハウスよりプレーヤーの合計値が高い or ハウスがバスト
 
 
-let table1 = Dealer.startGame(1, "poker");
-let table2 = Dealer.startGame(3, "21");
+// console.log("ブラックジャックを開始します。");
+// console.log(`あなたの引いたカードは${}の${}です。`);
+// console.log(`あなたの引いたカードは${}の${}です。`);
+// console.log(`ディーラーの引いたカードはダイヤのQです。`);
+// console.log("ディーラーの引いた2枚目のカードはわかりません。");
+// console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
+// console.log(`あなたの引いたカードは${}の${}です。`);
+// console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
+// console.log(`ディーラーの引いた2枚目のカードは${}の${}でした。`);
+// console.log(`ディーラーの現在の得点は${}です。`);
+// console.log(`ディーラーの引いたカードは${}の${}です`);
+// console.log(`あなたの得点は${}です。`);
+// console.log(`ディーラーの得点は${}です。`);
+// console.log("あなたの勝ちです!");
+// console.log("あなたの負けです!");
+// console.log("ブラックジャックを終了します。");
 
-Dealer.printTableInformation(table1);
-console.log(Dealer.checkWinner(table1));
 
-Dealer.printTableInformation(table2);
-console.log(Dealer.checkWinner(table2));
+// setTimeout(gameStart, 1000);
+
+// function gameStart() {
+//   let flag = false;
+//   let player1 = {};
+//   let player2 = {};
+//   let tern = 0;
+//   let player = "";
+//   while (!flag) {
+//     let card = 4
+//     console.log(`あなたの引いたカードは${card}です`);
+//     flag = true;
+//   }
+// }
+
+
+
+// 宅の情報を取得
+let table = Dealer.startGame(4, "21");
+
+Dealer.printTableInformation(table);
+// console.log(Dealer.winnerOf21(table));
+
+// let table1 = Dealer.startGame(1, "poker");
+// let table2 = Dealer.startGame(3, "21");
+
+// Dealer.printTableInformation(table1);
+// console.log(Dealer.checkWinner(table1));
+
+// Dealer.printTableInformation(table2);
+// console.log(Dealer.checkWinner(table2));
+
+import * as readline from 'readline/promises';
+
+(async () => {
+  const readInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const string = await readInterface.question("文字列を入力してください >");
+
+  console.log(string);
+
+  result(string)
+  readInterface.close();
+})();
+
+function result(string) {
+  if (string === "y" || string === "Y") {
+    console.log("結果はYです")
+  } else {
+    console.log("結果はNです")
+  }
+}
