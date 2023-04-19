a9be582687b2bb6c6cd35acb772f9cbd51f4df82
Takeshi Arihori <83809409+takeshi-arihori@users.noreply.github.com>
2023-04-19 10:03:12 +0900

    Update 18.md

--- quests/quests_week1-2/quest_blackjack/black_jack/app.mjs
+++ quests/quests_week1-2/quest_blackjack/black_jack/app.mjs
@@ -1,78 +1,100 @@
+import * as readline from 'readline/promises';
 import { HelperFunctions } from "./components/HelperFunctions.mjs";
 import { Card } from "./components/card.mjs";
 import { Dealer } from "./components/dealer.mjs";
+import { Deck } from "./components/deck.mjs";
 
 
 
-// console.log("ブラックジャックを開始します。");
-// console.log(`あなたの引いたカードは${}の${}です。`);
-// console.log(`あなたの引いたカードは${}の${}です。`);
-// console.log(`ディーラーの引いたカードはダイヤのQです。`);
-// console.log("ディーラーの引いた2枚目のカードはわかりません。");
-// console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
-// console.log(`あなたの引いたカードは${}の${}です。`);
-// console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
-// console.log(`ディーラーの引いた2枚目のカードは${}の${}でした。`);
-// console.log(`ディーラーの現在の得点は${}です。`);
-// console.log(`ディーラーの引いたカードは${}の${}です`);
-// console.log(`あなたの得点は${}です。`);
-// console.log(`ディーラーの得点は${}です。`);
+console.log("ブラックジャックを開始します。");
+
 // console.log("あなたの勝ちです!");
 // console.log("あなたの負けです!");
 // console.log("ブラックジャックを終了します。");
 
 
-// setTimeout(gameStart, 1000);
-
-// function gameStart() {
-//   let flag = false;
-//   let player1 = {};
-//   let player2 = {};
-//   let tern = 0;
-//   let player = "";
-//   while (!flag) {
-//     let card = 4
-//     console.log(`あなたの引いたカードは${card}です`);
-//     flag = true;
-//   }
-// }
 
 
+/* ============== 各クラスのテスト ============= */
 
-// 宅の情報を取得
-let table = Dealer.startGame(4, "21");
+/* ================= カードを取得 ================= */
+// let card1 = new Card("♥","J", 11);
+// console.log(card1) // Deckクラスに集約されている
 
-Dealer.printTableInformation(table);
+/* ================= デッキのカードを取得 ================= */
+// let deck1 = new Deck();
+// deck1.printDeck(); // Deck上の全てのカードを表示
+
+/* ================= シャッフルして再度表示 ================= */
+// let deck2 = new Deck();
+// console.log("スタート")
+// deck2.printDeck();
+// deck2.shuffleDeck();
+// console.log("シャッフルしてから再度表示")
+// deck2.printDeck();
+
+// ================= ディーラーを使ってプレイヤー全員にカードを配る処理 =================
+// カードやデッキはここで状態を保つが、ディーラーは状態を保たないステートレスオブジェクトになる
+// let table1 = Dealer.startGame(3, "21");
+// let table2 = Dealer.startGame(3, "poker")
+
+
+/* ================= テーブルの情報を受け取り、宅の情報を表示する ================= */
+// console.log("blackJack... \n")
+// Dealer.printTableInformation(table1);
+
+// console.log("poker... \n")
+// Dealer.printTableInformation(table2);
+
+
+/* ================= Playerのスコアの計算 ================= */
+// PlayerAの手札
+// let card1 = new Card("♦︎","A", 1);
+// let card2 = new Card("♦︎","J", 11);
+
+// // PlayerBの手札
+// let card3 = new Card("♦︎","9", 9);
+// let card4 = new Card("♦︎","K", 13);
+
+// console.log(Dealer.score21Individual([card1, card2]));
+// console.log(Dealer.score21Individual([card3, card4]));
+
+
+/* ================= 最も多いスコアを獲得したプレイヤー ================= */
+// 最大値は19(= index 2)
+// let arr1 = [1, 9, 19, 3, 4, 6];
+// console.log(HelperFunctions.maxInArrayIndex(arr1));
+
+// // 最大値は5(= index 0)
+// let arr2 = [5, 2, 1, 3, 5, 5];
+// console.log(HelperFunctions.maxInArrayIndex(arr2));
+
+
+/* ================= 最も多いスコアを獲得したプレイヤー ================= */
+// let table = Dealer.startGame(4, "21");
+
+// Dealer.printTableInformation(table);
 // console.log(Dealer.winnerOf21(table));
 
-// let table1 = Dealer.startGame(1, "poker");
-// let table2 = Dealer.startGame(3, "21");
 
+
+/* ================= 宅の種類によってトランプの値が変わる ================= */
+// let table = Dealer.startGame(4, "21");
+
+// Dealer.printTableInformation(table);
+// console.log(Dealer.winnerOf21(table));
+
+
+/* ================= 宅の種類によって勝利条件を変更する ================= */
+
+// let table1 = Dealer.startGame(1, "poker");
+let table2 = Dealer.startGame(3, "21");
+
+// console.log(" =================== table1 ======================")
 // Dealer.printTableInformation(table1);
 // console.log(Dealer.checkWinner(table1));
 
-// Dealer.printTableInformation(table2);
-// console.log(Dealer.checkWinner(table2));
 
-import * as readline from 'readline/promises';
-
-(async () => {
-  const readInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  const string = await readInterface.question("文字列を入力してください >");
-
-  console.log(string);
-
-  result(string)
-  readInterface.close();
-})();
-
-function result(string) {
-  if (string === "y" || string === "Y") {
-    console.log("結果はYです")
-  } else {
-    console.log("結果はNです")
-  }
-}
+// console.log(" =================== table2 ======================")
+Dealer.printTableInformation(table2);
+console.log(Dealer.checkWinner(table2));
--- quests/quests_week1-2/quest_blackjack/black_jack/components/Dealer.mjs
+++ quests/quests_week1-2/quest_blackjack/black_jack/components/Dealer.mjs
@@ -6,9 +6,9 @@ export class Dealer {
   static startGame(amountOfPlayers, gameMode) {
     // 卓の情報
     let table = {
-      "players": [],
-      "gameMode": gameMode,
-      "deck": new Deck(gameMode),
+      "players": [], // 二次元配列で各プレイヤーのカード情報を格納 [[{}, {}], [{}, {}], [{}, {}]]
+      "gameMode": gameMode, // poker or 21
+      "deck": new Deck(gameMode), // {}
     }
 
     // デッキをシャッフル
@@ -29,61 +29,109 @@ export class Dealer {
   }
 
   // ゲームの内容によって手札の枚数を変更する
-  static initialCards(gameMode){
-    if(gameMode == "poker") return 5;
-    if(gameMode == "21") return 2;
+  static initialCards(gameMode) {
+    if (gameMode == "poker") return 5;
+    if (gameMode == "21") return 2;
   }
 
   // 宅の情報を表示するメソッドを作成
-  static printTableInformation(table){
+  // ラウンドロビンで各indexごとに表示
+  static printTableInformation(table) {
     console.log("Amount of players: " + table["players"].length + "... Game mode: " + table["gameMode"] + ". At this table: ");
+    let flag = false;
+    let counter = 0;
+    let j = 0; // プレイヤーのカードを追跡(1周するごとにカウントアップ)
+    while (!flag) {
 
-    for(let i = 0; i < table["players"].length; i++){
-      console.log("Player " + (i + 1) + " hand is: ");
-      for(let j = 0; j < table["players"][i].length; j++){
-        console.log(table["players"][i][j].getCardString());
-      }
+      if(counter == 10) flag = true;
+      if(counter === table["players"].length) j++;
+      // for (let i = 0; i < table["players"].length; i++) {
+      //   console.log("Player " + (i + 1) + " hand is: ");
+      //   for (let j = 0; j < table["players"][i].length; j++) {
+      //     console.log(table["players"][i][j].getCardString());
+      //   }
+      // }
+      console.log('table["players"]')
+      console.log(`loop : ${j} 周目！`)
+      console.log(`ラウンドロビン : ${counter % table["players"].length} 回`)
+      console.log(table["players"][counter % table["players"].length][j])
+      /* ========= 自分(player1)のカードの2ペアを確認 ========= */
+      // console.log(`あなたの引いたカードは${myCardNumber}の${intValue}です。`);
+      // console.log(`あなたの引いたカードは${myCardNumber}の${intValue}です。`);
+
+
+      /* ========= その他のプレイヤー(player2-3)のカードの2ペアを確認 ========= */
+      // console.log(`プレイヤー${playerNumber}の引いたカードは${suit}の${intValue}です。`);
+      // console.log(`プレイヤー${playerNumber}の引いたカードは${suit}の${intValue}です。`);
+
+
+      /* ========= ディーラーの一枚目のカードだけ開示 ========= */
+      // console.log(`ディーラーの引いたカードはダイヤのQです。`);
+      // console.log("ディーラーの引いた2枚目のカードはわかりません。");
+
+      /* ========= 自分の現在のカードの合計を表示 and Game Out ========= */
+      // console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
+      // console.log(`あなたの引いたカードは${}の${}です。`);
+
+      /* ========= 再度カードを引くか確認 ========= */
+      // console.log(`あなたの現在の得点は${}です。カードを引きますか？（Y/N）`);
+      // console.log(`ディーラーの引いた2枚目のカードは${}の${}でした。`);
+
+      /* ========= CPUの機能について ========= */
+      // もし合計値が15以上ならもう一度引く
+
+      /* ========= カードを引くのをやめたらディーラーのカードを開示 ========= */
+      // console.log(`ディーラーの現在の得点は${}です。`);
+      // console.log(`ディーラーの引いたカードは${}の${}です`);
+
+
+
+      /* ========= 勝者が決定 ========= */
+      // console.log(`あなたの得点は${}です。`);
+      // console.log(`ディーラーの得点は${}です。`);
+      counter++;
     }
   }
 
   // 各プレイヤーの手札を受け取って、合計値を計算するメソッド
   // 21を超えると0になる
-  static score21Individual(cards){
+  static score21Individual(cards) {
     let value = 0;
-    for(let i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
       value += cards[i].intValue;
     }
-    if(value > 21) value = 0;
+    if (value > 21) value = 0;
     return value;
   }
 
   // 勝利したプレイヤーを表示する
   // それぞれのプレイヤーの手札をscore21Individualで計算
-  static winnerOf21(table){
+  static winnerOf21(table) {
     let points = [];
     let cache = [];
-    for(let i = 0; i < table["players"].length; i++){
+    for (let i = 0; i < table["players"].length; i++) {
       let point = Dealer.score21Individual(table["players"][i]);
       // それぞれのpointを配列に保存
       points.push(point);
 
-      if(cache[point] >= 1) cache[point] += 1;
+      if (cache[point] >= 1) cache[point] += 1;
       else cache[point] = 1;
     }
 
     // 各プレイヤーの得点を確認する
+    console.log("各プレイヤーの得点");
     console.log(points);
 
     let winnerIndex = HelperFunctions.maxInArrayIndex(points);
-    if(cache[points[winnerIndex]] > 1) return "It is a draw";
-    else if(cache[points[winnerIndex]] >= 0) return "player " + (winnerIndex + 1) + " is the winner";
+    if (cache[points[winnerIndex]] > 1) return "It is a draw";
+    else if (cache[points[winnerIndex]] >= 0) return "player " + (winnerIndex + 1) + " is the winner";
     else return "No winners..";
   }
 
 
   // 宅のゲームの種類によって勝利条件を変更
-  static checkWinner(table){
-    if(table["gameMode"] == "21") return Dealer.winnerOf21(table);
+  static checkWinner(table) {
+    if (table["gameMode"] == "21") return Dealer.winnerOf21(table);
     else return "no game";
   }
 }
